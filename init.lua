bb875378ec1563246a7f6e6d3ea0dbab3bf550f5
Danilo Tenorio Raisi <danilo@raisi.tech>
2023-12-21 16:41:44 +0000

    feat: Change colorscheme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin",
+  colorscheme = "tokyonight-moon",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
